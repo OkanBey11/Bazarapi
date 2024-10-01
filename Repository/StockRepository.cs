be6da070229013df1e2b405bb7be5a2820701e12
OkanBey11 <okangoztok@hotmail.com>
2024-10-01 13:49:07 +0300

    Pagenumber and Pagesize

--- Repository/StockRepository.cs
+++ Repository/StockRepository.cs
@@ -65,7 +65,9 @@ namespace api.Repository
                 }
             }
 
-            return await stock.ToListAsync();
+            var skipNumber = (query.PageNumber-1) * query.PageSize;
+
+            return await stock.Skip(skipNumber).Take(query.PageSize).ToListAsync();
         }
 
         public async Task<Stock?> GetByIdAsync(int id)
