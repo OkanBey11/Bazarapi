7fde69087c7a526f5d526d061a919175420642cd
OkanBey11 <okangoztok@hotmail.com>
2024-10-03 16:10:26 +0300

    Get And Create Portfolio

--- Program.cs
+++ Program.cs
@@ -61,6 +61,7 @@ builder.Services.AddAuthentication(optinos =>{
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 
 builder.Services.AddSwaggerGen(option =>
 {
